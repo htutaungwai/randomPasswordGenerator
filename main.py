c7ef68d619d09ab1ac77fa8f93c73cefd56b3195
htutaungwai <defo19987@gmail.com>
2026-01-27 16:39:40 +0630

    added final char array

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 import random
 import string
-
-length=int(input("Enter the password length:  "))
+player_length=int(input("Enter the password length:  "))
 
 def generate_password(length, use_digits, use_special):
     letters = string.ascii_letters
     digits = string.digits if use_digits else ""
     special = string.punctuation if use_special else ""
-    all_chars = letters + digits + special
+    # all_chars = letters + digits + special
 
-    print(digits)
-    print (all_chars)
+    password_chars = []
 
-generate_password(12, True, False)
+    if letters:
+        password_chars.append(random.choice(letters))
+    if use_digits and digits:
+        password_chars.append(random.choice(digits))
+    if use_special and special:
+        password_chars.append(random.choice(special))
+    print(password_chars)
+
+generate_password(12, True, True)
 
 
 
