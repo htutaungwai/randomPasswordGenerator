d52d32e44abbb42c08cd376e359c2d78dde20e98
htutaungwai <defo19987@gmail.com>
2026-02-03 12:35:42 +0630

    the random pool set up

--- main.py
+++ main.py
@@ -6,19 +6,28 @@ def generate_password(length, use_digits, use_special):
     letters = string.ascii_letters
     digits = string.digits if use_digits else ""
     special = string.punctuation if use_special else ""
-    all_chars = letters + digits + special
+    password = []
 
-    password_chars = []
 
-    if letters:
-        password_chars.append(random.choice(letters))
-    if use_digits and digits:
-        password_chars.append(random.choice(digits))
-    if use_special and special:
-        password_chars.append(random.choice(special))
-    print(password_chars)
+    for x in range (length):
+        pool = []
+        if letters:
+            pool.append(random.choice(letters))
+        if use_digits and digits:
+            pool.append(random.choice(digits))
+        if use_special and special:
+            pool.append(random.choice(special))
 
-generate_password(12, True, True)
+        if pool:
+            password.append(random.choice(pool))
+
+
+
+        print(password)
+
+
+
+generate_password(6, True, True)
 
 
 
